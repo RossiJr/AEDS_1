dfba37552e69ef52434b39b32dc3e6b36a4f1e29
RossiJr <jfrjunior@sga.pucminas.br>
2021-06-20 15:25:13 -0300

    Término dos exercícios

--- lista 14/main.cpp
+++ lista 14/main.cpp
@@ -1,31 +1,70 @@
 #include "classes.h"
 #include <cstdlib>
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
 using namespace std;
 
+void menu(int id){                  //Funcao para imprimir os menus
+    switch(id){
+        case 1:
+        cout<<"\n1- Ingresso normal\n2- Ingresso VIP\n0- Sair do menu\n";
+        break;
+        case 2:
+        cout<<"\n1- Camarote superior\n2- Camarote inferior\n0- Sair do menu\n";
+        break;
+    }
+    cout<<"Opcao: ";
+}
+
 int main(){
-    char teste1[20];
-    char teste2[20];
-    cin>>teste1;
-    cin>>teste2;
-    cout<<teste1 << " | "<<teste2;
-    Ingresso *ing = new Ingresso(500);
-    ing->imprimeValor();
+    //Declaracao de variaveis
+    float precoIngresso = 150;
+    float const valorExtra = 50;
 
-    IngressoVIP *ing2 = new IngressoVIP(100, 500);
-    ing2->imprimeValor();
+    int opcao1, opcao2;
+    char localizacaoSuperior[50] = "lado 1\0";
+    char localizacaoInferior[50] = "lado 2\0";
+    system("cls");
 
-    IngressoNormal *ing3 = new IngressoNormal(200);
-    ing3->imprimeValor();
-
-    CamaroteInferior *cam1 = new CamaroteInferior(teste1, 200, 300);
-    cam1->imprimeValor();
-    cam1->imprimirLocalizacaoInferior();
-
-    CamaroteSuperior *cam2 = new CamaroteSuperior(teste2, 100, 700);
-    cam2->imprimeValor();
-    cam2->imprimirLocalizacaoSuperior();
-
+    //inicio do menu 1
+    cout<<"Insira qual tipo de ingresso deseja: ";
+    menu(1);
+    do{                             //testes para valores validos
+    cin>>opcao1;
+        if(opcao1 == 1){
+            IngressoNormal *ingNormal = new IngressoNormal(precoIngresso);
+            ingNormal->imprimeValor();
+        }
+        else if(opcao1 == 0)
+            cout<<"Saindo do programa...\n";
+        else if(opcao1 == 2){
+            //inicio menu 2
+            cout<<"\nAgora insira o tipo de camarote:";
+            menu(2);
+            do{                     //testes para valores validos
+                cin>>opcao2;
+                if(opcao2 == 1){
+                    CamaroteSuperior *camSup = new CamaroteSuperior(localizacaoSuperior, valorExtra, precoIngresso);
+                    camSup->imprimeValor();
+                    camSup->imprimirLocalizacaoSuperior();
+                }
+                else if(opcao2 == 2){
+                    CamaroteInferior *camInf = new CamaroteInferior(localizacaoInferior, valorExtra, precoIngresso);
+                    camInf->imprimeValor();
+                    camInf->imprimirLocalizacaoInferior();
+                }
+                else if(opcao2 == 0){                   //encerramento do programa
+                    cout<<"\nSaindo do programa...";
+                    opcao1 = 0;                         //valor atribuido ao do-while do menu 1 para encerrar o programa
+                }
+                else
+                    cout<<"\nOpcao invalida, insira outra...\n";
+            }while(opcao2<0 || opcao2>2);
+        }
+        else
+            cout<<"\nOpcao invalida, insira outra...\n";
+    }while(opcao1<0 || opcao1>2);
 
     return 0;
 }
--- lista 14/metodos.cpp
+++ lista 14/metodos.cpp
@@ -3,32 +3,32 @@
 #include <iostream>
 using namespace std;
 
-Ingresso::Ingresso(float valorIngresso){
+Ingresso::Ingresso(float valorIngresso){                                                                                                                    //Construtor do objeto Ingresso
     this->valorIngresso = valorIngresso; 
 }
-void Ingresso::imprimeValor(){
+void Ingresso::imprimeValor(){                                                                                                                              //Método para imprimir valor do objeto Ingresso
     cout<<"\nValor do ingresso: "<<this->valorIngresso<<"\n";
 }
-void Ingresso::aumentarValorIngressoInicial(float valor){
+void Ingresso::aumentarValorIngressoInicial(float valor){                                                                                                   //Aumenta o valor do ingresso
     this->valorIngresso += valor;
 }
 
-IngressoVIP::IngressoVIP(float valorAdicional, float valorIngresso) : Ingresso(valorIngresso){
+IngressoVIP::IngressoVIP(float valorAdicional, float valorIngresso) : Ingresso(valorIngresso){                                                              //Construtor do Objeto IngressoVip
     this->aumentarValorIngressoInicial(valorAdicional);
 }
 
-IngressoNormal::IngressoNormal(float valorIngresso) : Ingresso(valorIngresso){}
+IngressoNormal::IngressoNormal(float valorIngresso) : Ingresso(valorIngresso){}                                                                             //Construtor do objeto Ingresso Normal
 
-CamaroteInferior::CamaroteInferior(char *localizacao, float valorAdicional, float valorIngresso) : IngressoVIP(valorAdicional, valorIngresso){
+CamaroteInferior::CamaroteInferior(char *localizacao, float valorAdicional, float valorIngresso) : IngressoVIP(valorAdicional, valorIngresso){              //Construtor do Objeto Camarote Inferior
     this->localizacaoInferior = localizacao;
 }
-void CamaroteInferior::imprimirLocalizacaoInferior(){
+void CamaroteInferior::imprimirLocalizacaoInferior(){                                                                                                       //Imprime a localizacao do camarote inferior - Poderia ser apenas no IngressoVip para gastar menos linhas de codigo
     cout<<"\nLocalizacao: "<<this->localizacaoInferior<<"\n";
 }
 
-CamaroteSuperior::CamaroteSuperior(char *localizacao, float valorAdicional, float valorIngresso) : IngressoVIP(valorAdicional, valorIngresso){
+CamaroteSuperior::CamaroteSuperior(char *localizacao, float valorAdicional, float valorIngresso) : IngressoVIP(valorAdicional, valorIngresso){              //Construtor do Objeto Camarote Superior
     this->localizacaoSuperior = localizacao;
 }
-void CamaroteSuperior::imprimirLocalizacaoSuperior(){
+void CamaroteSuperior::imprimirLocalizacaoSuperior(){                                                                                                       //Imprime a localizacao do camarote superior - Poderia ser apenas no IngressoVip para gastar menos linhas de codigo
     cout<<"\nLocalizacao: "<<this->localizacaoSuperior<<"\n";
 }
